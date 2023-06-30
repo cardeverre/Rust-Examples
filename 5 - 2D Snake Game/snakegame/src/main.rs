c91874e805b8bb38529c6163b8af6c60015f193b
JP van Loggerenberg <jploggies@gmail.com>
2023-06-30 20:56:50 +0200

    Started a 2D snake game

---
+++ 5 - 2D Snake Game/snakegame/src/main.rs
@@ -0,0 +1,14 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+fn main() {
+    println!("Hello, world!");
+}
